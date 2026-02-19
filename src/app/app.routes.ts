b7c550c301beb30ec13a1dd870c7fd3e2db9aa80
Andy <andysamuel2002lg@gmail.com>
2026-02-19 18:33:44 -0500

    canActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Pagina404 } from './shared/pagina-404/pagina-404';
-import { Servicios } from './shared/servicios/servicios';
 import { Mascotas } from './shared/mascotas/mascotas';
 import { Consultas } from './features/consultas/consultas';
 import { Usuarios } from './features/usuarios/usuarios';
-import { FormCuenta } from './shared/form-cuenta/form-cuenta';
 import { Login } from './shared/login/login';
 import { authGuard } from './guards/auth-guard';
+import { Formulario } from './shared/formulario/formulario';
+import { Acerca } from './features/acerca/acerca';
 
 export const routes: Routes = [
   // Rutas path vacías redirigen a la ruta principal
@@ -15,12 +15,12 @@ export const routes: Routes = [
   { path: '', component: Home },
 
   // Rutas para navegación
-  { path: 'acerca', component: Servicios },
+  { path: 'acerca', component: Acerca },
   { path: 'consultas', component: Consultas },
   { path: 'mascotas', component: Mascotas },
   // Protección con guardianes
-  { path: 'usuarios', component: Usuarios, canActivate:[authGuard]},
-  { path: 'cuenta', component: FormCuenta },
+  { path: 'usuarios', component: Usuarios, canActivate: [authGuard] },
+  { path: 'cuenta', component: Formulario },
   { path: 'login', component: Login },
 
   // Redirección para rutas no encontradas
